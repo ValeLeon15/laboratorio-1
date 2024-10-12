f4a4cb75c19428d2b5d130393a4820eee945cade
Larasalcedof <larasalcedof@gmail.com>
2024-10-12 12:44:34 -0500

    Creación de clases que implementan las interfaces, integración de sawggger

--- personapi-dotnet/Models/Interfaces/ITelefonoRepository.cs
+++ personapi-dotnet/Models/Interfaces/ITelefonoRepository.cs
@@ -6,11 +6,10 @@ namespace personapi_dotnet.Models.Interfaces
 {
     public interface ITelefonoRepository
     {
-        Task<IEnumerable<Telefono>> GetAllAsync();
-        Task<Telefono> GetByIdAsync(string numero);
+        Task<IEnumerable<Telefono>> GetAllAsync();  // Asegúrate de que el método es GetAllAsync si es asíncrono
+        Task<Telefono> GetByIdAsync(string num);
         Task AddAsync(Telefono telefono);
         Task UpdateAsync(Telefono telefono);
-        Task DeleteAsync(string numero);
-        string? GetAll();
+        Task DeleteAsync(string num);
     }
 }
--- personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
+++ personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d1595ad6be251c19dcb833eaae7a6db4cff9a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337f5fe1e09cb626cf3cb6557dae5c8ddd2db67e")]
 [assembly: System.Reflection.AssemblyProductAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
