b587639b2a9b0253772e551e00ef206ca030c6ff
Larasalcedof <larasalcedof@gmail.com>
2024-10-15 02:43:04 -0500

    Creacion carpeta build bd

--- personapi-dotnet/Program.cs
+++ personapi-dotnet/Program.cs
@@ -41,13 +41,14 @@ if (!app.Environment.IsDevelopment())
 }
 else
 {
-    // Habilitar Swagger solo en entorno de desarrollo
+    // Habilitar Swagger en cualquier entorno para pruebas
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-        c.RoutePrefix = string.Empty; // Swagger en la raíz
+        c.RoutePrefix = string.Empty;
     });
+
 }
 
 app.UseStaticFiles();
