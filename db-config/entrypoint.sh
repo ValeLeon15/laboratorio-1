d2b9c4aa868e587aeb7977d438b41f18934162ab
ValeLeon15 <vlg1529@gmail.com>
2024-10-18 11:42:48 -0500

    docker

--- db-config/entrypoint.sh
+++ db-config/entrypoint.sh
@@ -7,7 +7,8 @@ sleep 30s
  
 # Variables de entorno
 SA_PASSWORD=Password123!
- 
+
+
 # Ejecutar los scripts DDL y DML
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password123! -i /usr/src/app/persona_ddl_ms.sql
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password123! -i /usr/src/app/persona_dml_ms.sql
